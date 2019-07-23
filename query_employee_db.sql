b941246aa2321acc35ecaca2f7c38a5993de566e
ZGrinacoff <zgrinacoff@gmail.com>
2019-07-22 17:54:31 -0700

    Inital commit/main query

---
+++ query_employee_db.sql
@@ -0,0 +1,8 @@
+-- -- Check to confirm successful imports.
+-- SELECT * FROM departments;
+-- SELECT * FROM dept_emp;
+-- SELECT * FROM dept_manager;
+-- SELECT * FROM employees;
+-- SELECT * FROM salaries;
+-- SELECT * FROM titles;
+
